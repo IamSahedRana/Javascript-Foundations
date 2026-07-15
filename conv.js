ef3bad29bad8fb2dc7347bf80f9c699e117257d0
IamSahedRana <sahed.vmc@gmail.com>
2026-07-15 18:56:13 +0600

    Data Type Conversation and basic math opearation

---
+++ conv.js
@@ -0,0 +1,19 @@
+let a = "49";
+let b = parseInt(a);
+
+console.log(b);
+
+
+let c = "4830";
+let d = parseFloat(c);
+
+console.log(d);
+console.log(typeof d);
+
+
+const aa = .2;
+const bb = .1;
+
+const total = aa + bb ;
+
+console.log(total.toFixed(1));
