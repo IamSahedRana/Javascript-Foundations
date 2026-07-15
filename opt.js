b1810816bf5ba43bfcb0b651128b9ef92feacee5
IamSahedRana <sahed.vmc@gmail.com>
2026-07-15 19:10:16 +0600

    BASic Math Oprations

---
+++ opt.js
@@ -0,0 +1,21 @@
+let a = 50;
+let b = 49;
+
+let sum = a + b;
+let diff = a - b;
+let div = a / b;
+let multi = a * b;
+let expo = a ** b;
+let rem = a % b;
+
+console.log(sum);
+console.log(diff);
+console.log(div);
+console.log(multi);
+console.log(expo);
+console.log(rem);
+
+let price = 19 ;
+price = 49;
+
+console.log(++price);
